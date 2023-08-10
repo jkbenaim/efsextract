80414982c27410ef93f97aed1d4c3384435b35bd
jason <jason@noemail.net>
2023-08-10 01:12:04 +0000

    make progname shim work on irix
    FossilOrigin-Name: b15a18c29ea4c30c2abaec023ac3cc3296bdf4eae04b476d8661d44d7445ab2a

--- progname.c
+++ progname.c
@@ -1,13 +1,13 @@
 #include <string.h>
 #include "progname.h"
 
-#ifdef __MINGW32__
+#if defined(__MINGW32__) || defined(__sgi)
 char *__progname = NULL;
 #endif
 
 void progname_init(int argc, char *argv[])
 {
-#ifdef __MINGW32__
+#if defined(__MINGW32__) || defined(__sgi)
 	char *temp;
 	__progname = strrchr(argv[0], '\\');
 	if (!__progname) __progname = argv[0];
